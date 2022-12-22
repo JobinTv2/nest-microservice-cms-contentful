520fb64144e18861a1775584b7b1311ca93de28e
Jobin T v <jobintv@focaloid.com>
2022-12-22 17:49:15 +0530

    feat: Redis caching

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { BookController } from './book.controller';
 import { BookService } from './book.service';
-
+import * as redisStrore from 'cache-manager-redis-store';
 @Module({
+  imports: [
+    CacheModule.register({
+      store: redisStrore,
+      socket: { host: 'localhost', port: 6379 },
+    }),
+  ],
   controllers: [BookController],
   providers: [BookService],
 })
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,11 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { Cache } from 'cache-manager';
 @Injectable()
 export class BookService {
+  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+
   async getForm() {
     const result = await axios(
       `${process.env.CONTENTFUL_CONTENT_DELIVERY_BASE_URL}/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_ENVIRONMENT_ID}/entries?sys.id=TyA16n8unGpZ3uuuHHwYA&access_token=${process.env.CONTENTFUL_CONTENT_DELIVERY_TOKEN}`,
     );
-    return result.data;
+    // await this.cacheManager.reset();
+    let data = await this.cacheManager.get('book-form-data');
+
+    if (!data) {
+      console.log(data, 'data');
+      await this.cacheManager.set('book-form-data', result.data, 10);
+      data = await this.cacheManager.get('book-form-data');
+    }
+    return data;
   }
 }
